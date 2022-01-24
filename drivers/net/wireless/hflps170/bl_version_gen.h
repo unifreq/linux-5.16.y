cc347d82f513a4786c7d3ac0b60455a30f012f88
unifreq <flippy@sina.com>
2022-01-24 19:59:00 +0800

    remove drivers/net/wireless/hflps170/bl_version_gen.h

--- drivers/net/wireless/hflps170/bl_version_gen.h
+++
@@ -1,3 +0,0 @@
-#define BL_VERS_REV "git5345cdadfd7d8c2e0dc75cda10024b854e15cc9cM (5345cdadf/main)"
-#define BL_VERS_MOD "5.5.0.0"
-#define BL_VERS_BANNER "bl v5.5.0.0 - build: root Jan 24 2022 17:22:31 - git5345cdadfd7d8c2e0dc75cda10024b854e15cc9cM (5345cdadf/main)"
